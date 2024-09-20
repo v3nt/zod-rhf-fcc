03c7fe220ae40912a44416a526890e6b1b3eb0c8
v3nt <dan@jynk.net>
2024-09-20 12:24:51 +0100

    setup running and types fixed

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -1,5 +1,68 @@
-function Form() {
-  return <></>;
-}
+import { useForm } from "react-hook-form";
+import FormField from "./FormField";
+
+const Form = ({ }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+    setError,
+  } = useForm<FormData>({ defaultValues: "" });
+
+  const onSubmit = async (data: FormData) => {
+    console.log("SUCCESS", data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <h1>Zod & React Hook form</h1>
+        <FormField
+          type="email"
+          placeholder="Email"
+          name="email"
+          register={register}
+          error={errors.email}
+        />
+
+        <FormField
+          type="text"
+          placeholder="GitHub URL"
+          name="githubUrl"
+          register={register}
+          error={errors.githubUrl}
+        />
+
+        <FormField
+          type="number"
+          placeholder="Years of Experience (1 - 10)"
+          name="yearsOfExperience"
+          register={register}
+          error={errors.yearsOfExperience}
+          valueAsNumber
+        />
+
+        <FormField
+          type="password"
+          placeholder="Password"
+          name="password"
+          register={register}
+          error={errors.password}
+        />
+
+        <FormField
+          type="password"
+          placeholder="Confirm Password"
+          name="confirmPassword"
+          register={register}
+          error={errors.confirmPassword}
+        />
+        <button type="submit" className="submit-button">
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+};
 
 export default Form;
--- app/components/FormField.tsx
+++ app/components/FormField.tsx
@@ -1,5 +1,21 @@
-function FormField() {
-  return <></>;
+import { FormFieldProps } from "@/types";
+
+const FormField: React.FC<FormFieldProps> = ({
+  type,
+  placeholder,
+  name,
+  register,
+  error,
+  valueAsNumber,
+}) => {
+  return (<>
+    <input
+      type={type}
+      placeholder={placeholder}
+      {...register(name, { valueAsNumber })}
+    />
+    {error && <span className="error-message">{error.message}</span>}
+  </>);
 }
 
 export default FormField;

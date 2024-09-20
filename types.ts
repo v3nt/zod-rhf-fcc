9c1331f4b07e7db727b5a0045a7fa0c0a524b659
v3nt <dan@jynk.net>
2024-09-20 12:37:40 +0100

    zod schema setup

--- types.ts
+++ types.ts
@@ -1,25 +1,52 @@
 import { FieldError, UseFormRegister } from "react-hook-form";
+import { z, ZodType } from "zod";
 
 export type FormData = {
-    email: string;
-    githubUrl: string;
-    yearsOfExperience: number;
-    password: string;
-    confirmPassword: string;
-}
+  email: string;
+  githubUrl: string;
+  yearsOfExperience: number;
+  password: string;
+  confirmPassword: string;
+};
 
 export type FormFieldProps = {
-    type: string;
-    placeholder: string;
-    name: ValidFieldNames;
-    register: UseFormRegister<FormData>;
-    error: FieldError | undefined;
-    valueAsNumber?: boolean;
-}
+  type: string;
+  placeholder: string;
+  name: ValidFieldNames;
+  register: UseFormRegister<FormData>;
+  error: FieldError | undefined;
+  valueAsNumber?: boolean;
+};
 
 export type ValidFieldNames =
-    | "email"
-    | "githubUrl"
-    | "yearsOfExperience"
-    | "password"
-    | "confirmPassword";
+  | "email"
+  | "githubUrl"
+  | "yearsOfExperience"
+  | "password"
+  | "confirmPassword";
+
+const requiredFieldMessage = "Required field";
+
+export const UserSchema: ZodType<FormData> = z
+  .object({
+    email: z.string().email(),
+    githubUrl: z
+      .string()
+      .url()
+      .includes("github.com", { message: "Invalid GitHub URL" }),
+    yearsOfExperience: z
+      .number({
+        required_error: requiredFieldMessage,
+      })
+      .min(1)
+      .max(10),
+    password: z
+      .string()
+      .min(8, { message: "Password is too short" })
+      .max(20, { message: "Password is too long" }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"], // path of error
+  });
